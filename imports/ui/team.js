fd455c81a354f2a7f30973a9ef9dcaabeae517a7
Amy Ewbank <amyewbank@Robins-MacBook-Pro.local>
2017-05-19 17:04:46 -0500

    amy's commit

--- imports/ui/team.js
+++ imports/ui/team.js
@@ -13,6 +13,7 @@ Template.team.events({
 
 Template.team.helpers({
    pct(id) {
+    /this helper gives the percentage win-loss, counting ties as half a win/
        
         var total = 0;
         var wins = 0;
@@ -39,4 +40,78 @@ Template.team.helpers({
         return (wins / total);
            
    },
+   wins(id) {
+    /this helper gives the total number of wins (not counting ties)/
+       
+        var total = 0;
+        var wins = 0;
+        
+        var matches = Matches.find({});
+        
+        
+        matches.forEach((match) => {
+            if(match.t1 === id || match.t2 === id){
+                total += 1;
+                
+                if (match.t1 === id && match.s1 > match.s2) {
+                    wins += 1;
+                } else if (match.t2 === id && match.s2 > match.s1) {
+                    wins += 1;
+                }
+            }
+        });
+        
+        if(total === 0)
+            return 0.0;
+        return (wins);           
+   },
+   ties(id) {
+    /this helper gives the number of ties only/
+       
+        var total = 0;
+        var ties = 0;
+        
+        var matches = Matches.find({});
+        
+        
+        matches.forEach((match) => {
+            if(match.t1 === id || match.t2 === id){
+                total += 1;
+                
+                if(match.s1 === match.s2){
+                    ties += 1;
+                }
+            }
+        });
+        
+        if(total === 0)
+            return 0.0;
+        return ties;      
+   },
+   losses(id) {
+    /this helper gives the number of losses by subtracting the number of wins from the total/
+       
+        var total = 0;
+        var wins = 0;
+        
+        var matches = Matches.find({});
+        
+        
+        matches.forEach((match) => {
+            if(match.t1 === id || match.t2 === id){
+                total += 1;
+                
+                if (match.t1 === id && match.s1 > match.s2) {
+                    wins += 1;
+                } else if (match.t2 === id && match.s2 > match.s1) {
+                    wins += 1;
+                }
+            }
+        });
+        
+        if(total === 0)
+            return 0.0;
+        return (total - wins);           
+   },
+
 });
